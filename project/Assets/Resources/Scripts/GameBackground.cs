6a72d7256a322253d781c887c6d9ded54f7963fe
Toshiki Kado <cad.tsk@gmail.com>
2015-01-17 17:50:02 +0900

    ゲームシーンに背景を追加

---
+++ project/Assets/Resources/Scripts/GameBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBackground : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpd = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (0.0f, Camera.main.gameObject.transform.position.y * moveSpd);						// Yの値がずれていくオフセットを作成.
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);	// マテリアルにオフセットを設定する.
+	}
+}
