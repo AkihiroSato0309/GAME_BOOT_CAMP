4cdf37f4dcada9dd7e18cc56c4885f2d0e6bb003
Toshiki Kado <cad.tsk@gmail.com>
2015-01-17 22:14:50 +0900

    ゲームシーンにタッチ数ゲージの追加

---
+++ project/Assets/TouchGauge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TouchGauge : MonoBehaviour {
+
+	public int recoverGaugeFrame;
+	private float recoverNum;
+	private float currentAmount = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		recoverNum = 1.0f / recoverGaugeFrame;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentAmount += recoverNum;
+		gameObject.GetComponent<Image> ().fillAmount = currentAmount;
+
+		if (currentAmount >= 1.0f) {
+			// Add touch stack gauge
+			Destroy (gameObject);
+		}
+	}
+}
