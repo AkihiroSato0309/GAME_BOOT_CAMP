9f98a4ba7d27b2965044973145e28ed2b9ebd04b
Akihiro <gaigurensetuna@gmail.com>
2015-01-17 12:32:37 +0900

    もろもろ設定

--- project/Assets/Resources/Scripts/Ball.cs
+++ project/Assets/Resources/Scripts/Ball.cs
@@ -2,14 +2,50 @@
 using System.Collections;
 
 public class Ball : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+//========================================================================================
+// 定数
+//========================================================================================
 	
+	
+//========================================================================================
+// 変数
+//========================================================================================
+	//--public----------------------
+	public float m_maxPower = 1.0f;
+	
+	//--pirvate---------------------
+	private Camera m_mainCamera;
+	
+//========================================================================================
+// 関数
+//========================================================================================
+	//--------------------------------------------------------
+	// 初期化処理
+	//--------------------------------------------------------
+	void Start () 
+	{
+		m_mainCamera = Camera.main;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	//--------------------------------------------------------
+	// 更新処理
+	//--------------------------------------------------------
+	void Update () 
+	{
+		// タッチ時の処理
+		if(Input.GetMouseButtonDown(0))
+		{
+			Vector2 mousePos = Input.mousePosition;
+			Vector3 worldMousePos = m_mainCamera.ScreenToWorldPoint(
+				new Vector3(mousePos.x, mousePos.y, 0.0f));
+
+			float dist = Vector3.Distance(worldMousePos, gameObject.transform.position);
+
+			Vector3 addForceVelocity = gameObject.transform.position - worldMousePos;
+			addForceVelocity.Normalize();
+			addForceVelocity *= m_maxPower;
+
+
+		}
 	}
 }
