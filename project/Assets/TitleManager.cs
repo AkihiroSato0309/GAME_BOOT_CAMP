593ab00a9b621bb20ce2cd691a25c03fabd5247b
Toshiki Kado <cad.tsk@gmail.com>
2015-01-18 16:43:04 +0900

    シーン関係の修正

---
+++ project/Assets/TitleManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("BestScore").GetComponent<Text> ().text = DataSaver.GetHighestScore ().ToString () + "m";
+		GameObject.Find ("Coin").GetComponent<Text> ().text = DataSaver.GetCoinNum ().ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
