63fba5c77e18be76742262e7b27916b076930d84
Toshiki Kado <cad.tsk@gmail.com>
2015-01-18 09:33:59 +0900

    ゲームシーンのUI完成

--- project/Assets/Resources/Scripts/UI/Timer.cs
+++ project/Assets/Resources/Scripts/UI/Timer.cs
@@ -9,19 +9,29 @@ public class Timer : MonoBehaviour {
 	private int count = 60;
 	private int COUNT_MIN = 0;
 	private int COUNT_MAX = 60;
+	private bool isActive = true;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isActive) return;
+
 		if (--count <= COUNT_MIN) {
 			--second;
 			count = COUNT_MAX;
 		}
 
 		gameObject.GetComponent<Text> ().text = second.ToString ();
+	}
+
+	public void StopUpdate () {
+		isActive = false;
+	}
+
+	public void StartUpdate () {
+		isActive = true;
 	}
 }
